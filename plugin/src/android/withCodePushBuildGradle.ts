2bb9ea01dbec739f5c5b100ce1d339790400bdb9
Yusuf Emirhan Sahin <syusufemirhan@gmail.com>
2022-09-07 16:58:29 +0300

    build gradle fix

--- plugin/src/android/withCodePushBuildGradle.ts
+++ plugin/src/android/withCodePushBuildGradle.ts
@@ -2,8 +2,7 @@ import fs from 'fs/promises';
 import { ExpoConfig } from '@expo/config-types';
 import { AndroidConfig, ConfigPlugin, withAppBuildGradle, withDangerousMod, withSettingsGradle } from "@expo/config-plugins";
 
-const gradleModules = `apply from: "../../node_modules/react-native/react.gradle"
-apply from: "../../node_modules/react-native-code-push/android/codepush.gradle"`
+const gradleModules = `apply from: "../../node_modules/react-native-code-push/android/codepush.gradle"`
 
 const modifyAppBuildGradle = (contents: string): string => {
   if (!contents.includes(gradleModules)) {
